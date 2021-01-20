3cf251cf4754db15b9b14701d6bf67abe5747a52
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-01-20 21:09:06 +0100

    feat: base

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!--    <img alt="Vue logo" src="./assets/logo.png" />-->
+    <CssCake />
+  </div>
+</template>
+
+<script>
+import CssCake from "./components/CssCake.vue";
+
+export default {
+  name: "App",
+  components: {
+    CssCake
+  }
+};
+</script>
+
+<style lang="stylus">
+body
+  background-color #946def
+
+#app
+  display flex
+  justify-content center
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  margin-top 60px
+</style>
---
+++ src/components/CssCake.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="birthday-cake">
+    <div class="cake"></div>
+    <div class="base">
+      <div class="base-1"></div>
+      <div class="base-2"></div>
+      <div class="base-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CssCake"
+};
+</script>
+
+<style lang="stylus">
+.birthday-cake
+  .base
+    .base-1
+      width 450px
+      height 30px
+      background-color black
+      border-bottom-left-radius 30px
+      border-bottom-right-radius 30px
+
+    .base-2
+      width 100px
+      height 50px
+      margin 0 auto
+      background-color black
+
+    .base-3
+      width 200px
+      height 30px
+      margin 0 auto
+      border-top-left-radius 10px
+      border-top-right-radius 10px
+      background-color black
+</style>
